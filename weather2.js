b2fab3c226af49b90041221efc531d650b91120e
Brian Holland <neumaics@gmail.com>
2015-02-21 11:13:00 -0700

    Redoing loop.

--- weather2.js
+++ weather2.js
@@ -4,24 +4,30 @@ var wire = new i2c(address, {device: '/dev/i2c-1'});
 
 var tempBlocking = 0xE3;
 
-wire.write([tempBlocking], function(err) {
-  if (err) {
-    console.log(err);
-    throw err;
-  }
+function getTemp() {
+  wire.write([tempBlocking], function(err) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
 
-  setTimeout(function() {
-    wire.read(3, function(err, res) {
-      if (err) {
-        console.log(err);
-        throw err;
-      } else {
-        var temperature = (res[0] << 8) | res[1];
-        temperature = temperature & 0xFFFC;
+    setTimeout(function() {
+      wire.read(3, function(err, res) {
+        if (err) {
+          console.log(err);
+          throw err;
+        } else {
+          var temperature = (res[0] << 8) | res[1];
+          temperature = temperature & 0xFFFC;
 
-        temperature = -46.85 + (175.72 * (temperature / 65536));
-        console.log(temperature);
-      }
-    });
-  }, 50);
-});
+          temperature = -46.85 + (175.72 * (temperature / 65536));
+          console.log(temperature);
+        }
+      });
+    }, 50);
+  });
+}
+
+setInterval(function(){
+  getTemp();
+}, 1000);
