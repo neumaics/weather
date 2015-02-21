0627055b2604d68ce9331802f618147b3d5d9b65
Brian Holland <neumaics@gmail.com>
2015-02-20 23:39:26 -0700

    Reverting loop

--- weather2.js
+++ weather2.js
@@ -4,33 +4,24 @@ var wire = new i2c(address, {device: '/dev/i2c-1'});
 
 var tempBlocking = 0xE3;
 
-function getTemperature() {
-  wire.write([tempBlocking], function(err) {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
+wire.write([tempBlocking], function(err) {
+  if (err) {
+    console.log(err);
+    throw err;
+  }
 
-    setTimeout(function() {
-      wire.read(3, function(err, res) {
-        if (err) {
-          console.log(err);
-          throw err;
-        } else {
-          var temperature = (res[0] << 8) | res[1];
-          temperature = temperature & 0xFFFC;
-
-          temperature = -46.85 + (175.72 * (temperature / 65536));
-          console.log(temperature);
-        }
-      });
-    }, 50);
-  });
-}
-
-
-for (var i=0; i < 10; i++) {
   setTimeout(function() {
-    getTemperature();
-  }, 10);
-}
+    wire.read(3, function(err, res) {
+      if (err) {
+        console.log(err);
+        throw err;
+      } else {
+        var temperature = (res[0] << 8) | res[1];
+        temperature = temperature & 0xFFFC;
+
+        temperature = -46.85 + (175.72 * (temperature / 65536));
+        console.log(temperature);
+      }
+    });
+  }, 50);
+});
