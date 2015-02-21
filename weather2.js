27992e9c93fab91bab9f455ed1e62baa75d4615b
Brian Holland <neumaics@gmail.com>
2015-02-20 18:48:20 -0700

    Basic looping.

--- weather2.js
+++ weather2.js
@@ -4,24 +4,33 @@ var wire = new i2c(address, {device: '/dev/i2c-1'});
 
 var tempBlocking = 0xE3;
 
-wire.write([tempBlocking], function(err) {
-  if (err) {
-    console.log(err);
-    throw err;
-  }
+function getTemperature() {
+  wire.write([tempBlocking], function(err) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
 
+    setTimeout(function() {
+      wire.read(3, function(err, res) {
+        if (err) {
+          console.log(err);
+          throw err;
+        } else {
+          var temperature = (res[0] << 8) | res[1];
+          temperature = temperature & 0xFFFC;
+
+          temperature = -46.85 + (175.72 * (temperature / 65536));
+          console.log(temperature);
+        }
+      });
+    }, 50);
+  });
+}
+
+
+for (var i=0; i < 10; i++) {
   setTimeout(function() {
-    wire.read(3, function(err, res) {
-      if (err) {
-        console.log(err);
-        throw err;
-      } else {
-        var temperature = (res[0] << 8) | res[1];
-        temperature = temperature & 0xFFFC;
-
-        temperature = -46.85 + (175.72 * (temperature / 65536));
-        console.log(temperature);
-      }
-    });
-  }, 50);
-});
+    getTemperature();
+  }, 10);
+}
