354b808f033c475c78b798211b99059d2d313fe6
Brian Holland <neumaics@gmail.com>
2015-02-22 12:25:43 -0700

    Stubbed some requests

--- app/server.js
+++ app/server.js
@@ -9,6 +9,16 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;
 var router = express.Router();
 
+router.
+  post('/temperature', function (req, res) {
+    res.send('Got a POST request');
+  }).
+
+  get('/temperature', function(req, res) {
+    res.send('Got a GET request');
+  });
+
 app.use('/api', router);
 
 app.listen(port);
+console.log('running on port ', port);
